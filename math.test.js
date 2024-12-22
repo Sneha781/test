603da3c3bf1cebb250918b3f8d9cfd14d39e68d1
Sneha781 <snehalatha.ikutti@qwikspark.com>
2024-12-22 09:39:01 +0530

    Update math.test.js

--- math.test.js
+++ math.test.js
@@ -1,9 +1,14 @@
-const add = require('../../dev/math');
+function add(a, b) {
+    return a + b;
+}
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+// New function
+function subtract(a, b) {
+    return a - b;
+}
 
-});
+module.exports = { add, subtract };
+
 
 
 
